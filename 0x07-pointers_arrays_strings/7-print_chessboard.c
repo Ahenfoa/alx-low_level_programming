7bf379f827b2d931c3aba3183c4cdfcfe793e676
Ahenfoa <izanrry@gmail.com>
2023-08-14 15:30:00 +0000

    fix

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * print_chessboard - Entry point
+ * print_chessboard - starting point
  * @a: array
  * Return: Always 0 (Success)
 */
@@ -9,7 +9,7 @@ void print_chessboard(char (*a)[8])
 	int c;
 	int b;
 
-	for (a = 0; c < 8; c++)
+	for (c = 0; c < 8; c++)
 	{
 
 		for (b = 0; b < 8; b++)
