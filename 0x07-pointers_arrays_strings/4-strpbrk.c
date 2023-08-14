c22f439fc8bdf7c9c8c8653371ba06f82335da7a
Ahenfoa <izanrry@gmail.com>
2023-08-14 15:26:15 +0000

    fixing

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -1,1 +1,24 @@
+#include "main.h"
+/**
+ * _strpbrk - starting point
+ * @s: input
+ * @accept: input
+ * Return: returns o when succesful
+*/
+char *_strpbrk(char *s, char *accept)
+{
 
+	int a;
+
+	while (*s)
+	{
+		for (a = 0; accept[a]; a++)
+		{
+		if (*s == accept[a])
+		return (s);
+		}
+		s++;
+		}
+
+	return ('\0');
+}
--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,1 +1,28 @@
+#include "main.h"
+/**
+* _strstr - starting point
+* @haystack: input
+* @needle: input
+* Return: return o when sucessful
+*/
+char *_strstr(char *haystack, char *needle)
+{
 
+	for (; *haystack != '\0'; haystack++)
+	{
+
+		char *a = haystack;
+		char *b = needle;
+
+		while (*a == *b && *b != '\0')
+		{
+			a++;
+			b++;
+		}
+
+		if (*b == '\0')
+		return (haystack);
+	}
+
+	return (0);
+}
