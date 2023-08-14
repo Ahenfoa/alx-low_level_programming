aac2091784e4c152b8bf42bd23bae1dcea0a4cf7
Ahenfoa <izanrry@gmail.com>
2023-08-14 15:47:00 +0000

    point

--- 0x07-pointers_arrays_strings/100-set_string.c
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -1,1 +1,11 @@
+#include "main.h"
 
+/**
+ * set_string - sets the value of a pointer to a char
+ * @s: points to pointer
+ * @to: points to char
+*/
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -1,1 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_diagsums - starting point
+ * @a: input
+ * @size: input
+ * Return: returns o successful
+*/
+void print_diagsums(int *a, int size)
+{
+	int add1, add2, z;
 
+	add1 = 0;
+	add2 = 0;
+
+	for (z = 0; z < size; z++)
+	{
+		add1 = add1 + a[z * size + z];
+	}
+	for (z = size - 1; z >= 0; z--)
+	{
+		add2 += a[z * size + (size - z - 1)];
+	}
+	printf("%d, %d\n", add1, add2);
+}
