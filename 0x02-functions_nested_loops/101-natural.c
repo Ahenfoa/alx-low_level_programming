a39aceb6f09c9a793ab996fe76da32bddc73a9e6
Ahenfoa <izanrry@gmail.com>
2023-10-22 00:06:53 +0000

    5-sign.c

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -14,16 +14,19 @@
 int main(void)
 {
 
-	int i, sum = 0;
+	int i; //sum = 0;
 
 	for (i = 0; i < 1024; i++)
-	{
+	/*{
 
 		if ((i % 3) == 0 || (i % 5) == 0)
 			sum += i;
-	}
+	}*/
 
-	printf("%d\n", sum);
+	//printf("%d\n", sum);
+	{
+		printf("%d\n",i);
+	}
 
 	return (0);
 }
