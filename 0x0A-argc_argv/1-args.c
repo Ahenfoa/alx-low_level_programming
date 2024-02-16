2332af1e0a496d1b7c90308bd1f54381fa3749fe
Ahenfoa <izanrry@gmail.com>
2024-02-16 16:34:55 +0000

    1-args.c

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,27 +1,15 @@
 #include <stdio.h>
 #include "main.h"
 /**
-* main: Print the number of cl arguments passed to the program
-* @ argc: number of arguments
-* @ argv: array of argument
+* main - prints the number of arguments passed into it
+* @argc: number of command line arguments
+* @argv: array of command line arguments
 *
-* Return: returns 0 on success
+* Return: 0 if successful
 */
-
-
 int main(int argc, char *argv[])
 {
-if (argc < 2)
-{
-printf("Usage: %s argument1 [argument2...]\n", argv[0]);
-return (1);
-}
-
-for (int i = i < argc; i++)
-{
-	printf("Argument %d: %s\n", i, argv[i]);
-}
-return (0);
-
-
+	(void)argv;
+	printf("%d\n", argc - 1);
+	return (0);
 }
